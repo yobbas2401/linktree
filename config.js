532fb746df06689b0a41b1eabba084dc13ec8763
Geovani <104106944+yobbas2401@users.noreply.github.com>
2023-10-16 13:04:03 -0600

    v1

---
+++ config.js
@@ -0,0 +1,10 @@
+export let config = {
+    name: 'Geovani Velazquez VEGA',
+    imageURL: 'FOTO.JPG',
+    links: [
+        { title: 'LINKEDIN', url: 'https://www.linkedin.com/in/geovani2401' },
+        { title: 'GITHUB', url: 'https://example.com/' },
+        { title: 'WORDPRESS', url: 'https://gscircuits.wordpress.com/' },
+        { title: 'PORTAFOLIO', url: 'https://gscircuits.wordpress.com/portafolio-solidworks/' },
+    ]
+}

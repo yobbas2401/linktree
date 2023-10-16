68bbe118b81844f57f3e2fedf782e941475e82d8
Geovani <104106944+yobbas2401@users.noreply.github.com>
2023-10-16 13:31:14 -0600

    V2

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 export let config = {
     name: 'Geovani Velazquez Vega',
-    imageURL: './FOTO.JPG',
     links: [
         { title: 'LINKEDIN', url: 'https://www.linkedin.com/in/geovani2401' },
         { title: 'GITHUB', url: 'https://example.com/' },
